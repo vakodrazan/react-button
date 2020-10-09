ac58c26936b3a547a0655a9d17936821490c682a
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-09 11:13:39 +0300

    Use a different variables for each conditions because if just using one variables, it just overwrite the style

--- Button.js
+++ Button.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 function Button(props) {
-    let buttonStyle;
-    if (props.variant == "outline") {
-        buttonStyle= "outline"
-    } else if (props.variant == "text") {
-        buttonStyle= "text"
-    } else if (props.disableShadow) {
-        buttonStyle= "disabledShadow"
-    } else if (props.disabled) {
-        buttonStyle= "disabled"
-    } 
+
+    const variantOutline = props.variant == "outline" ? "outline" : "";
+    const variantText = props.variant == "text" ? "text" : "";
+    const disabledShadow = props.disableShadow ? "disabledShadow" : "";
+    const buttonDisabled = props.disabled ? "disabled" : "";
 
     return (
         <div>
             <p>{props.paragraph}</p>
             <button 
-                className={buttonStyle}
+                className={`
+                    ${variantOutline} 
+                    ${variantText} 
+                    ${disabledShadow}
+                    ${buttonDisabled}
+                `}
             >Default</button>
         </div>
     )
